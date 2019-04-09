e63e29bc149e6a352ba2448fc3ab6314ddfbdce0
William Eiffert <weiffert@gmail.com>
2019-04-09 19:30:06 -0400

    Create App page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import "./App.css";
+import { auth } from "./firebase";
+import SignIn from "./SignIn";
 
 class App extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    const login = window.localStorage.getItem("login");
+    if (login !== "null") {
+      this.handleAuth(login);
+    }
+
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.handleAuth(user.uid);
+      } else {
+        this.signOut();
+      }
+    });
+  }
+
+  handleAuth = user => {
+    this.setState({ user });
+    window.localStorage.setItem("login", this.state.user);
+  };
+
+  signedIn = () => {
+    return this.state.user;
+  };
+
+  signOut = () => {
+    window.localStorage.removeItem("login");
+    this.setState({ user: null });
+    auth.signOut();
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Switch>
+          <Route
+            path="/sign-in"
+            render={navProps =>
+              this.signedIn() ? (
+                <Redirect to="/workspace" />
+              ) : (
+                <SignIn {...navProps} />
+              )
+            }
+          />
+          <Route
+            path="workspace"
+            render={navProps =>
+              this.signedIn() ? <p>signedin</p> : <Redirect to="/signin" />
+            }
+          />
+          <Route
+            render={navProps =>
+              this.signedIn() ? (
+                <Redirect to="/workspace" />
+              ) : (
+                <Redirect to="/sign-in" />
+              )
+            }
+          />
+        </Switch>
       </div>
     );
   }
