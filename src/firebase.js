d9a8f1f231d3f60b2dcbc6c349f579112df749a1
William Eiffert <weiffert@gmail.com>
2019-04-09 19:29:31 -0400

    Setup firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,10 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/firestore'
 import config from "./config.js";
 
-firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+const firestore = firebase.firestore();
 
-export default firebase;
+export const auth = app.auth();
+export const githubAuthProvider = new firebase.auth.GithubAuthProvider();
