4cc91325f0b91c5553f0cd0b30c8f4e49f886942
William Eiffert <weiffert@gmail.com>
2019-04-15 09:41:38 -0400

    Add selects for determining roles.

--- src/CreateSprint.js
+++ src/CreateSprint.js
@@ -11,6 +11,10 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Avatar from "@material-ui/core/Avatar";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 import FindUser from "./FindUser";
 import UserList from "./UserList";
@@ -18,33 +22,44 @@ import Stepper from "./Stepper";
 
 class CreateSprint extends React.Component {
   state = {
-    newName: "",
-    newID: "",
-    checked: [],
-    activeStep: 0
+    activeStep: 0,
+    scrumMaster: null,
+    productOwner: null,
+    team: []
   };
 
-  handleChange = name => event => {
+  componentDidUpdate(prevProps) {
+    if (prevProps.open === false && this.props.open === true) {
+      this.setState({
+        activeStep: 0,
+        scrumMaster: null,
+        productOwner: null,
+        team: []
+      });
+    }
+  }
+
+  handleChange = event => {
     this.setState({
-      [name]: event.target.value
+      [event.target.name]: event.target.value
     });
   };
 
   updateChecked = checked => {
-    this.setState({ checked });
+    this.setState({ team: checked });
   };
 
   getSteps = () => {
-    return ["Choose a workspace name", "Invite users", "Create!"];
+    return ["Determine Roles", "Upload Artifacts", "Schedule Events", "Start!"];
   };
 
   handleNext = () => {
     if (this.state.activeStep + 1 === this.getSteps().length) {
-      this.props.functions.create.workspace({
+      this.props.functions.create.sprint({
         name: this.state.newName,
         users: this.state.checked
       });
-      this.props.callback();
+      this.props.handleClose();
     } else {
       this.setState({
         activeStep: this.state.activeStep + 1
@@ -62,34 +77,59 @@ class CreateSprint extends React.Component {
     switch (step) {
       case 0:
         return (
-          <div>
-            <TextField
-              id="name"
-              name="name"
-              label="Workspace Name"
-              required={true}
-              onChange={this.handleChange("newName")}
-              margin="normal"
-              variant="outlined"
-              inputProps={{ name: this.state.newName }}
-            />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <FormControl variant="outlined" style={{ minWidth: 120 }}>
+              <InputLabel htmlFor="productOwner">Product Owner</InputLabel>
+              <Select
+                value={this.state.productOwner}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: "productOwner",
+                  id: "productOwner"
+                }}
+              >
+                {this.props.users.map(u => (
+                  <MenuItem value={u.id} key={u.id}>
+                    {u.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl style={{ minWidth: 120 }}>
+              <InputLabel htmlFor="scrumMaster">Scrum Master</InputLabel>
+              <Select
+                value={this.state.scrumMaster}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: "scrumMaster",
+                  id: "scrumMaster"
+                }}
+              >
+                {this.props.users.map(u => (
+                  <MenuItem key={u.id} value={u.id}>
+                    {u.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <div>
+              <Typography variant="h5">Select Team Members</Typography>
+              <UserList
+                users={this.props.users}
+                checkable={true}
+                updateChecked={this.updateChecked}
+              />
+            </div>
           </div>
         );
       case 1:
-        return (
-          <div>
-            <UserList
-              checkable={true}
-              updateChecked={this.updateChecked}
-              users={this.props.globals.users}
-            />
-            <FindUser functions={this.props.functions} />
-          </div>
-        );
+        return <div />;
       case 2:
+        return <div />;
+      case 3:
         return (
           <div>
-            <Typography>{`Your new workspace: ${
+            <Typography>{`Your new sprint: ${
               this.state.newName
             }`}</Typography>
             <Typography>{"Users"}</Typography>
@@ -105,21 +145,24 @@ class CreateSprint extends React.Component {
   render() {
     const steps = this.getSteps();
     return (
-      <Dialog onClose={this.props.handleClose} open={this.props.open}>
+      <Dialog
+        onClose={this.props.handleClose}
+        open={this.props.open}
+        fullWidth={true}
+        maxWidth="md"
+      >
         <DialogTitle id="Create Workspace">Create Workspace</DialogTitle>
         <Stepper
           getSteps={this.getSteps}
           getStepContent={this.getStepContent}
           continue={() =>
-            this.state.activeStep === 0 && this.state.newName === ""
+            this.state.scrumMaster === null ||
+            this.state.productOwner === null ||
+            this.state.team.length === 0
           }
-          finish={() => {
-            this.props.functions.create.workspace({
-              name: this.state.newName,
-              users: this.state.checked
-            });
-            this.props.handleClose();
-          }}
+          activeStep={this.state.activeStep}
+          handleNext={this.handleNext}
+          handleBack={this.handleBack}
           cancel={this.props.handleClose}
         />
       </Dialog>
